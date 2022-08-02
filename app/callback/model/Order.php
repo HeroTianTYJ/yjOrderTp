f7efc03c885175255f1e3ed2de6b0a58dc5fe534
HeroTianTYJ <48871307+HeroTianTYJ@users.noreply.github.com>
2022-08-02 14:23:03 +0800

    删除无用的查询

--- app/callback/model/Order.php
+++ app/callback/model/Order.php
@@ -2,25 +2,10 @@
 
 namespace app\callback\model;
 
-use Exception;
 use think\Model;
 
 class Order extends Model
 {
-    //查询一条
-    public function one($orderId = 0)
-    {
-        try {
-            return $this->field('user_id,product_id,price,count,name,tel,province,city,county,town,address,' .
-                'wxxcx_openid,date')
-                ->where(['order_id' => $orderId])
-                ->find();
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            return [];
-        }
-    }
-
     //修改支付状态
     public function modify($orderId, $paymentId, $payId = 0, $payScene = '', $payDate = '')
     {
