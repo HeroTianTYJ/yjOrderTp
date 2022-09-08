ad71fff0fdd5a71fe013ecc0363e982b5abbcc03
HeroTianTYJ <48871307+HeroTianTYJ@users.noreply.github.com>
2022-09-08 14:34:33 +0800

    版本更新

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '30.0|2022-05-22',  //版本
+    'version' => '35.0|2022-09-08',  //版本
     'page_size' => 20,  //默认每页条数
 
     'demo' => $_SERVER['HTTP_HOST'] == 'www.yjrj.top' && getUserIp() != '111.225.42.206',
