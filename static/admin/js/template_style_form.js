d0950908416582125503709248911fec60c6b5e3
HeroTianTYJ <48871307+HeroTianTYJ@users.noreply.github.com>
2024-01-04 10:29:11 +0800

    解决文件名大小写问题

--- static/admin/js/template_style_form.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-  $('input[name=bg_color]').colorPicker();
-  $('input[name=border_color]').colorPicker();
-  $('input[name=button_color]').colorPicker();
-  $('input[name=select_current_bg_color]').colorPicker();
-});
